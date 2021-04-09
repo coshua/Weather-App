0265ce62fa5f10c224c09a359836c554967e8346
coshua <bitlez3470@gmail.com>
2021-04-09 15:03:57 -0700

    mobile icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
 import React from "react";
-import './Home.css';
-import { Link } from 'react-router-dom'
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <>
-   Home 
-  <div className="mobile">
-    <Link to="/weather">
-      <button>Click</button>
-    </Link>
-  </div>
-  </>;
+  return (
+    <>
+      Home
+      <div className="mobile">
+        <Link to="/weather">
+          <button style={{ backgroundColor: "white", border: "none" }}>
+            <img alt="touch this icon" src="/android-chrome-192x192.png" />
+          </button>
+        </Link>
+        <p>Click the icon</p>
+      </div>
+    </>
+  );
 };
 
 export default Home;
